3339c55e55d4c571feed1572217142d0e0d66700
Juan Villegas Muñoz <juan.ma44602@cesurformacion.com>
2021-09-23 12:30:46 +0200

    ejercicio 4 parte 2

--- Ejercicio4parte2.php
+++ Ejercicio4parte2.php
@@ -7,10 +7,12 @@
     <title>Document</title>
 </head>
 <body>
+
+<table border="1">
     <?php
 
-    "<table>";
-    
+   
+    /*
     for ($i=0; $i <= 5 ; $i++) { 
         
         for ($x=0; $x < 6; $x++) { 
@@ -32,7 +34,48 @@
             }
         }
   
+  */
+
   
+  for ($i=0; $i <= 5 ; $i++) { 
+        
+    for ($x=0; $x < 6; $x++) { 
+        $matriz[$i][$x]= rand(-5,5);
+        
+    }
+    
+}
+$positivo=0;
+$negativo=0;
+$cero=0;
+
+    for ($i=0; $i < 5 ; $i++) { 
+        echo "<tr>";
+        for ($x=0; $x < 6; $x++) { 
+            echo "<td>" . $matriz[$i][$x] . "</td>";
+            if ($matriz[$i][$x]<0) {
+                
+                $negativo++;
+                
+            }else if ($matriz[$i][$x]==0) {
+                
+                $cero++;
+                
+            }else {
+                
+                $positivo++;
+                
+            }
+
+        }
+        echo "</tr>";
+    }
+
+    echo "</table>";
+
+    echo "<p>Hay $positivo numeros positivos</p>";
+    echo "<p>Hay $negativo numeros negativos</p>";
+    echo "<p>Hay $cero numeros ceros</p>";
 
 
     ?>
