4c36f79aab5c1bdde741463842b6ff46dc6a57eb
Juan Villegas Muñoz <juan.villegasm21@gmail.com>
2021-09-27 13:14:35 +0200

    Ejercicio 2 arreglado

--- tema 1 parte 2/Ejercicio2parte2.php
+++ tema 1 parte 2/Ejercicio2parte2.php
@@ -9,7 +9,31 @@
 <body>
     <?php
 
-    echo rand(1,6);
+
+// Arreglar en casa.
+    $dado = rand(1,6);
+
+    switch ($dado) {
+        case '1':
+            echo "Uno";
+            break;
+        case '2':
+            echo "Dos";
+           break;
+        case '3':
+        echo "Tres";
+            break;
+        case '4':
+        echo "Cuatro";
+            break;
+        case '5':
+        echo "Cinco";
+            break;
+        case '6':
+        echo "Seis";
+            break;     
+        
+    }
 
     ?>
 </body>
