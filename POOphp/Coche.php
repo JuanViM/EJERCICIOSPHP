966b006053dba1d1b6b9c47dd53b25a47182ef82
Juanillo21 <juan.ma44602@cesurformacion.com>
2021-10-04 10:48:59 +0200

    coche arreglado

--- POOphp/Coche.php
+++ POOphp/Coche.php
@@ -1,119 +1,119 @@
 <?php
 
 
-    class coche{
+class coche
+{
 
-        private $tipo_Combustible;
-        private $cantidadCombustible;
-        private $velocidad;
-        private $capacidadcombustible;
-        private $litrosreserva;
+    private $tipo_Combustible;
+    private $cantidadCombustible;
+    private $velocidad;
+    private $capacidadcombustible;
+    private $litrosreserva;
 
     //getters
-        function getTipoCombustible(){
+    function getTipoCombustible()
+    {
 
-            return $this->tipo_Combustible;
-
-        }
-
-        function getcantidadCombustible(){
-
-            return $this->cantidadCombustible;
-
-        }
-        function getVelocidad(){
-
-            return $this->velocidad;
-
-        }
-
-        function getCapacidadCombustible(){
-
-            return $this->capacidadcombustible;
-        }
-
-        function getLitrosReserva(){
-
-            return $this->litrosreserva;
-        }
-        
-        //Setters
-
-        function setTipo_Combustible($tipo_Combustible){
-
-            $this->tipo_Combustible = $tipo_Combustible;
-
-        }
-
-        function cantidadCombustible($cantidadCombustible){
-
-            $this->cantidadCombustible = $cantidadCombustible;
-
-        }
-
-        function velocidad($velocidad){
-
-            $this->velocidad = $velocidad;
-
-        }
-
-        function capacidadcombustible($capacidadcombustible){
-
-            $this->capacidadcombustible = $capacidadcombustible;
-
-        }
-
-        function litrosreserva($litrosreserva){
-
-            $this->litrosreserva = $litrosreserva;
-
-        }
-
-        //funciones
-
-        function movimiento(){
-
-            if ($this->velocidad>0) {
-                return true;
-            }
-
-            return false;
-
-        }
-
-        function seEncuentraReserva(){
-            
-            if ($this->cantidadCombustible<=$this->litrosreserva) {
-                return true;
-            }
-
-            return false;
-
-        }
-
-        function acelerar(){
-
-            $this->velocidad = $this->velocidad++;
-
-        }
-
-        function frenar(){
-
-            $this->velocidad = $this->velocidad--;
-
-
-        }
-
-
-        function repostar(){
-            if ($this->movimiento()==true) {
-                return false;
-            }
-
-            $this->cantidadCombustible = $this->cantidadCombustible++;
-
-        }
-
-
-
+        return $this->tipo_Combustible;
     }
+
+    function getcantidadCombustible()
+    {
+
+        return $this->cantidadCombustible;
+    }
+    function getVelocidad()
+    {
+
+        return $this->velocidad;
+    }
+
+    function getCapacidadCombustible()
+    {
+
+        return $this->capacidadcombustible;
+    }
+
+    function getLitrosReserva()
+    {
+
+        return $this->litrosreserva;
+    }
+
+    //Setters
+
+    function setTipo_Combustible($tipo_Combustible)
+    {
+
+        $this->tipo_Combustible = $tipo_Combustible;
+    }
+
+    function cantidadCombustible($cantidadCombustible)
+    {
+
+        $this->cantidadCombustible = $cantidadCombustible;
+    }
+
+    function velocidad($velocidad)
+    {
+
+        $this->velocidad = $velocidad;
+    }
+
+    function capacidadcombustible($capacidadcombustible)
+    {
+
+        $this->capacidadcombustible = $capacidadcombustible;
+    }
+
+    function litrosreserva($litrosreserva)
+    {
+
+        $this->litrosreserva = $litrosreserva;
+    }
+
+    //funciones
+
+    function movimiento()
+    {
+
+        if ($this->velocidad > 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    function seEncuentraReserva()
+    {
+
+        if ($this->cantidadCombustible <= $this->litrosreserva) {
+            return true;
+        }
+
+        return false;
+    }
+
+    function acelerar()
+    {
+
+        $this->velocidad++;
+    }
+
+    function frenar()
+    {
+
+        $this->velocidad--;
+    }
+
+
+    function repostar()
+    {
+        if ($this->movimiento() == true) {
+            return false;
+        }
+        while ($this->capacidadcombustible < $this->cantidadCombustible) {
+            $this->cantidadCombustible++;
+        }
+    }
+}
