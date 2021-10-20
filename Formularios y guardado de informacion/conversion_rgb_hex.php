63cfced5694467eb89354d25865391e4ce8ae808
Juanillo21 <juan.ma44602@cesurformacion.com>
2021-10-20 10:14:01 +0200

    Ejercicio caso practico 2 acabado y entregado

--- Formularios y guardado de informacion/conversion_rgb_hex.php
+++ Formularios y guardado de informacion/conversion_rgb_hex.php
@@ -1,15 +1,6 @@
 <?php
  
 
-
-echo $_POST["color1"] . "<br>";
-echo $_POST["color2"]. "<br>";
-echo $_POST["color3"]. "<br>";
-
-echo dechex($_POST["color1"]). "<br>";
-echo dechex($_POST["color2"]). "<br>";
-echo dechex($_POST["color3"]). "<br>";
-
-echo $hexa ="#". (dechex($_POST["color1"]) . dechex($_POST["color2"]).dechex($_POST["color3"]));
+echo $hexa =(dechex($_POST["color1"]) . dechex($_POST["color2"]).dechex($_POST["color3"]));
 
 ?>
