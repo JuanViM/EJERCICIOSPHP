fb61aeb20e3fd9042f8faccb356d154837055539
Juanillo21 <juan.ma44602@cesurformacion.com>
2021-11-10 13:08:22 +0100

    empezando el 5

---
+++ basedatos_conexion/Caso_Practico5/principal.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Elija que opcion desea ver, los actores o las peliculas</p>
+    
+    
+       <a href="actores.php>">Actores</a></option>
+        <option value="peliculas">Peliculas</option>
+    </select>
+    
+    
+</body>
+</html>
