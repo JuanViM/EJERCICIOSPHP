78f86901af57ef1f1ebd29dcc6f58bfcdc6ee092
Juanillo21 <juan.ma44602@cesurformacion.com>
2021-11-15 12:59:07 +0100

    mvc ejemplos a terminar

---
+++ MVC/Tarea_ejemplo/escuela.php
@@ -0,0 +1,13 @@
+<?php
+
+class Escuela{
+    //atributos para la conexion
+
+    private $local = "localhost";
+    private $user = "root";
+    private $pass = "";
+    private $port = 3007;
+
+}
+
+?>
